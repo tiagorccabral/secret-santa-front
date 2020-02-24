600b5802014d55d22caf1f602a2c9e931c3d3d4b
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-02-24 19:34:12 -0300

    Adiciona form inicial

--- src/pages/Auth/Signup/index.js
+++ src/pages/Auth/Signup/index.js
@@ -1,24 +1,103 @@
 import React, {Component} from 'react';
-import {Card, Col, Container, Row} from "react-bootstrap";
+import {Form, Button, Card, Col, Container, Row} from "react-bootstrap";
 
 class Signup extends Component {
-    render () {
+    state = {
+        email: "",
+        first_name: "",
+        last_name: "",
+        password: "",
+        password_confirmation: ""
+    };
+
+    handleUserInput(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]: value});
+    }
+
+    render() {
         return (
-            <div
-                style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}
-            >
-                <Container
-                >
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <Container>
                     <Row className="justify-content-xl-center">
-                        <Col xl="auto">
-                            <Card className="text-center">
+                        <Col xl="8">
+                            <Card>
                                 <Card.Body>
-                                    <Card.Title>Crie sua conta no Criador de Amigos Ocultos</Card.Title>
+                                    <Card.Title className="text-center">Crie sua Conta</Card.Title>
                                     <Card.Text/>
-                                    <Card.Text>
-                                        ou
-                                    </Card.Text>
-                                    <Card.Link href="/login">Faça login</Card.Link>
+                                    <Form>
+                                        <Form.Group controlId="formBasicEmail">
+                                            <Form.Label>E-mail</Form.Label>
+                                            <Form.Control
+                                                value={this.state.email}
+                                                onChange={e => this.handleUserInput(e)}
+                                                required
+                                                type="email"
+                                                name="email"
+                                                placeholder="email@gmail.com"
+                                            />
+                                            <Form.Text className="text-muted">
+                                                Por favor insira um e-mail válido.
+                                            </Form.Text>
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formFirstName">
+                                            <Form.Label>Nome</Form.Label>
+                                            <Form.Control
+                                                value={this.state.first_name}
+                                                onChange={e => this.handleUserInput(e)}
+                                                required
+                                                type="text"
+                                                name="first_name"
+                                                placeholder="Beto"
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formLastName">
+                                            <Form.Label>Sobrenome</Form.Label>
+                                            <Form.Control
+                                                value={this.state.last_name}
+                                                onChange={e => this.handleUserInput(e)}
+                                                required
+                                                type="last_name"
+                                                name="last_name"
+                                                placeholder="Carreiro"
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formBasicPassword">
+                                            <Form.Label>Senha</Form.Label>
+                                            <Form.Control
+                                                value={this.state.password}
+                                                onChange={e => this.handleUserInput(e)}
+                                                required
+                                                type="password"
+                                                name="password"
+                                                placeholder="Senha"
+                                            />
+                                        </Form.Group>
+                                        <Form.Group controlId="formBasicPasswordConfirm">
+                                            <Form.Label>Confirme sua senha</Form.Label>
+                                            <Form.Control
+                                                value={this.state.password_confirmation}
+                                                onChange={e => this.handleUserInput(e)}
+                                                required
+                                                type="password"
+                                                name="password_confirmation"
+                                                placeholder="Senha"
+                                            />
+                                        </Form.Group>
+                                        <Button variant="success" type="submit">
+                                            Cadastrar
+                                        </Button>
+                                    </Form>
+                                    <div className="text-center">
+                                        <Card.Text>
+                                            ou
+                                        </Card.Text>
+                                        <Card.Link href="/login">Faça login</Card.Link>
+                                    </div>
                                 </Card.Body>
                             </Card>
                         </Col>
